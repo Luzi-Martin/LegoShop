adb405d197334eb46f55a4f00e5a69b6b490f5f8
Scholer Lani <bschla@bbc-be.local>
2019-12-03 16:30:23 +0100

    createt registry

--- templates/user/registration.php
+++ templates/user/registration.php
@@ -1,32 +1,42 @@
 <div class="row">
+	<div>$title</div>
 	<form action="/user/registrate" method="post" class="col-6">
 		<div class="form-group">
-		  <label for="fname">Vorname</label>
-	  	<input id="fname" name="fname" type="text" class="form-control">
+			<label for="fname">Vorname</label>
+			<input id="fname" name="fname" type="text" class="form-control">
 		</div>
 		<div class="form-group">
-		  <label for="lname">Nachname</label>
-	  	<input id="lname" name="lname" type="text" class="form-control">
+			<label for="lname">Nachname</label>
+			<input id="lname" name="lname" type="text" class="form-control">
 		</div>
 		<div class="form-group">
-		  <label for="email">Mail</label>
-	  	<input id="email" name="email" type="text" class="form-control">
+			<label for="email">Mail</label>
+			<input id="email" name="email" type="text" class="form-control">
 		</div>
 		<div class="form-group">
-		  <label for="street">Strasse</label>
-	  	<input id="street" name="street" type="text" class="form-control">
+			<label for="street">Strasse</label>
+			<input id="street" name="street" type="text" class="form-control">
 		</div>
 		<div class="form-group">
-		  <label for="house_nr">Haus Nummer</label>
-	  	<input id="house_nr" name="house_nr" type="text" class="form-control">
+			<label for="house_nr">Haus Nummer</label>
+			<input id="house_nr" name="house_nr" type="text" class="form-control">
 		</div>
 		<div class="form-group">
-		  <label for="location">Ort</label>
-	  	<input id="location" name="location" type="text" class="form-control">
+			<label for="location">Ort</label>
+			<input id="location" name="location" type="text" class="form-control">
 		</div>
 		<div class="form-group">
 			<label class="control-label" for="password">Passwort</label>
 			<input id="password" name="password" type="password" class="form-control">
+		</div>
+		<div class="form-group">
+			<select class="form-control form-control-sm">
+				<?php
+					foreach ($view->locations as $key => $value) {
+						echo "<option>$value</option>";
+					}
+				?>
+			</select>
 		</div>
 		<button type="submit" name="send" class="btn btn-primary">Registrieren</button>
 	</form>
