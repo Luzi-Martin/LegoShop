ceeca1a0eb38b32cf2836696a62dfe9d51841cce
Luzi Martin <luzimartin9@gmail.com>
2019-12-09 11:42:07 +0100

    Added logout functionality and own page for logout

--- templates/user/logout.php
+++ templates/user/logout.php
@@ -1,5 +1,10 @@
 <?php
-    //session_start();
-    unset($_SESSION['loggedin']);
-    unset($_SESSION['user'] ['email']);
 ?>
+<div class="row">
+	<h1>Du wurdest erfolgreich abgemeldet</h1>
+</div>
+<div class="row">
+	<div >
+		<a href="/">Hier kommen sie wieder zur Startseite</a>
+	</div>
+</div>
