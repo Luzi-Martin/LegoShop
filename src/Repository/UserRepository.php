aab77106b68b385a9477544dbb25ad47e8413ddd
Scholer Lani <bschla@bbc-be.local>
2019-12-09 08:47:33 +0100

    made Registration

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -28,17 +28,54 @@ class UserRepository extends Repository
      * @param $lastName Wert für die Spalte lastName
      * @param $email Wert für die Spalte email
      * @param $password Wert für die Spalte password
-     *
+     * @param $street Wert für die Spalte street
+     * @param $house_nr Wert für die Spalte house_nr
+     * @param $location_id Wert für die Spalte location_id
+     * 
      * @throws Exception falls das Ausführen des Statements fehlschlägt
      */
     public function create($firstName, $lastName, $email, $password, $street, $hous_nr, $location_id)
     {
+        /// Injection Handling
+
+        if (preg_match('/(<|>|"|\')/', $firstName)) {
+            return;
+        }
+
+        if (preg_match('/(<|>|"|\')/', $lastName)) {
+            return;
+        }
+
+        if (preg_match('/(<|>|"|\')/', $email)) {
+            return;
+        }
+
+        if (preg_match('/(<|>|"|\')/', $password)) {
+            return;
+        }
+
+        if (preg_match('/(<|>|"|\')/', $street)) {
+            return;
+        }
+
+        if (preg_match('/(<|>|"|\')/', $hous_nr)) {
+            return;
+        }
+
+        if (preg_match('/(<|>|"|\')/', $location_id)) {
+            return;
+        }
+
+
+
+
+        $false = 0;
         $password = sha1($password);
 
-        $query = "INSERT INTO $this->tableName (firstName, lastName, email, password, false, street, house_nr, location_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $query = "INSERT INTO $this->tableName (firstName, lastName, email, password, admin, street, house_nr, location_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
         $statement = ConnectionHandler::getConnection()->prepare($query);
-        $statement->bind_param('ssssisii', $firstName, $lastName, $email, $password, false, $street, $hous_nr, 1);
+        $statement->bind_param('ssssissi', $firstName, $lastName, $email, $password,  $false, $street, $hous_nr, $location_id);
 
         if (!$statement->execute()) {
             throw new Exception($statement->error);
