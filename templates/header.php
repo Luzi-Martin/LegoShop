5d624fd8d9f1bf1eaa3f00f374ea725bf20d8ed8
Luzi Martin <luzimartin9@gmail.com>
2019-12-09 12:45:54 +0100

    fixed logo in top left corner (header)

--- templates/header.php
+++ templates/header.php
@@ -17,7 +17,7 @@ session_start();
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="H:\Lego-shop\LegoShop\public\favicon.ico" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="favicon.ico" width="30" height="30" class="d-inline-block align-top" alt="">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
