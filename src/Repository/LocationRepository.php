0ac1526a334450737a86a9d238daf42a5bc9cadd
Jigme7 <Tseten007@yahoo.de>
2019-12-16 11:52:12 +0100

    - bug fixing

---
+++ src/Repository/LocationRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repository;
+
+use App\Database\ConnectionHandler;
+use Exception;
+
+/**
+ * Das UserRepository ist zuständig für alle Zugriffe auf die Tabelle "user".
+ *
+ * Die Ausführliche Dokumentation zu Repositories findest du in der Repository Klasse.
+ */
+class LocationRepository extends Repository
+{
+    protected $tableName = 'locations';
+
+}
